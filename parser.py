8c91f9709dfe928e608af49a24f4ff5d267e8b79
KonstantinosStefanidis <viviotic01@gmail.com>
2026-03-30 23:42:02 +0300

    Start of project

---
+++ parser.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+import re
+from datetime import datetime
+
+def parser(filepath):
+    failedAtempts = defaultdict(list)
+    pattern = r"(\w{3}\s+\d+\s+\d+:\d+:\d+).*Failed password.*from\s+(\d+\.\d+\.\d+\.\d+)"
+    
+
+    with open(filepath, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if "Failed password" in line:
+                match = re.search(pattern, line)
+                if match:
+                    timestampStr = match.group(1)
+                    ip = match.group(2)
+                    dateParsed = datetime.strptime(timestampStr, "%b %d %H:%M:%S")
+                    failedAtempts[ip].append(dateParsed)
+
+    return failedAtempts
+                    
